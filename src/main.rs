290f92bb25a8d5df2811a5d579a7bbcd0c34711b
Dmitrii Demenev <demenev.dmitriy1@gmail.com>
2023-09-24 03:08:24 -0600

    Applied cargo rdme

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+//! # `cargo-fill`
+//!
+//! An interactive CLI tool for filling the fields of `Cargo.toml` quickly.
+//!
+//! ## Installation
+//!
+//! ```console
+//! cargo install cargo-fill
+//! ```
+//!
+//! ## Usage
+//!
+//! ```console
+//! cargo fill
+//! ```
+//!
+//! ## Features
+//!
+//! * Fills all known fields in the `[package]` section of `Cargo.toml`.
+//! * Allows using `cargo-msrv` to fill the `rust-version` field.
+//! * Allows using `git config --get <user.name|user.email|remote.origin.url>` to guess the `authors` and `repository` fields.
+//!
+//! ## License
+//!
+//! Licensed under either of [Apache License, Version 2.0] or [MIT license] at your option.
+//!
+//! [Apache License, Version 2.0]: https://www.apache.org/licenses/LICENSE-2.0
+//! [MIT license]: https://opensource.org/licenses/MIT
+
 use cargo_toml::{Inheritable, OptionalFile, Package};
 use std::path::PathBuf;
 // TODO: replace promptly with a prompt library
